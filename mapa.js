0f777cdcf95ade14f22007aa8fee9dddff0cfbf3
diegoaraaujo <seu-email@example.com>
2024-02-26 17:16:32 -0300

    first commit

---
+++ mapa.js
@@ -0,0 +1,11 @@
+function mostrarmapa(){
+    let local = document.getElementById("localizacao")
+    local.style.display = 'block'
+}
+
+function fecharmapa(){
+    let ocultar = document.getElementById("localizacao")
+    ocultar.style.display = 'none'
+}
+
+
