b532bc570ccccb7b44329e3b2e982445fd65148e
diegoaraaujo <seu-email@example.com>
2024-02-27 18:01:33 -0300

    horarios adicionados

--- mapa.js
+++ mapa.js
@@ -8,4 +8,17 @@ function fecharmapa(){
     ocultar.style.display = 'none'
 }
 
+function verrecado(){
+    let recado = document.getElementById("recado-horario")
+    recado.style.display = 'block'
+}
 
+function fecharrecado(){
+    let recado = document.getElementById("recado-horario")
+    recado.style.display = 'none'
+}
+
+function fecharrecado2(){
+    let recado2 = document.getElementById("recado-horario")
+    recado2.style.display = 'none'
+}
